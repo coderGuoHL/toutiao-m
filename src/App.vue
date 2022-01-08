f2fd19e427ad79b78c17535950fd01cea5a4aa02
coderGuoHL <2396420165@qq.com>
2022-01-08 12:28:24 +0800

    项目初始化-引入vant组件

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,33 @@
     <div>
       <i class="toutiao toutiao-wuwangluo"></i>
     </div>
+    <div>111</div>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell is-link @click="showPopup">展示弹出层</van-cell>
+    <van-popup v-model:show="show">内容</van-popup>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-  name: 'app'
+  name: 'app',
+  setup () {
+    const show = ref(false)
+    const showPopup = () => {
+      show.value = true
+    }
+    return {
+      show,
+      showPopup
+    }
+  }
 }
 </script>
 
