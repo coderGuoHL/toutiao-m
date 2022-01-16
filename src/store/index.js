c597ada9a0e2a1efc00efa3768a72fc049733ee1
coderGuoHL <2396420165@qq.com>
2022-01-16 22:44:39 +0800

    1

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    // 定义一个user用来存储user相关的数据
+    user: null
+
   },
   mutations: {
+    setUser (state, data) {
+      console.log(data)
+      state.user = data
+    }
   },
   actions: {
   },
